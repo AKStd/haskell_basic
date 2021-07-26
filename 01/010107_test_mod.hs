bebe71854751960d3639cc99b0b784e63e3ca4b6
Aleksandr <karpov.a1exandr@yandex.ru>
2021-07-26 19:08:57 +0300

    lesson 1.1 done

---
+++ 01/010107_test_mod.hs
@@ -0,0 +1,9 @@
+-- пример
+-- олсо :load - загрузить скрипт в интерпретаторе (пишем имя файла)
+-- :reload перезагрузить файл (если были изменения) 
+
+
+
+module Test where
+
+    sayHello = putStrLn "Hello from Test module"
