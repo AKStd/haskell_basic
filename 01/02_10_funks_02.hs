36a859165974224c118159b86ff838539d4d24eb
Aleksandr <karpov.a1exandr@yandex.ru>
2021-07-28 03:13:29 +0300

    opers done

--- 01/02_10_funks_02.hs
+++ 01/02_10_funks_02.hs
@@ -10,7 +10,11 @@ sign2 x
   | x > 0 = 1
   | otherwise = 0
 
--- частично заполненые функции
--- let max5 x = max 5 x
--- or
--- let max5' = max 5
+{-
+
+частично заполненые функции
+let max5 x = max 5 x
+or
+let max5' = max 5
+
+-}
